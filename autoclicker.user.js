1323d8aceae9550498b7307ae0d6aca122392126
Joe Kluch <joe.kluchinski@gmail.com>
2022-04-02 16:16:00 -0400

    forgot to update the coords on the autoclicker

--- autoclicker.user.js
+++ autoclicker.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Hunt Showdown rplace autoclicker
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  support clicking
 // @author       jkluch
 // @match        https://hot-potato.reddit.com/embed*
